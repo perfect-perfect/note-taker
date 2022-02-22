68d7c677333f46d0546f34a64b14a6f156687c8f
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-02-22 18:24:49 -0500

    setting up express

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server now on port: ${PORT}`);
+});
+
